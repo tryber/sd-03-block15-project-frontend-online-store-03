99fae0ee5b641b3700fb23f98410ba5545fd3011
Wenderson belko <57719936+wendersonbelko@users.noreply.github.com>
2020-05-12 16:43:14 -0300

    code climate

--- src/services/rotas.jsx
+++ src/services/rotas.jsx
@@ -1,12 +1,1 @@
-import React from 'react';
-import { HashRouter } from 'react-router-dom';
-
-class Rotas extends React.Component {
-  render() {
-    return (
-      <h1>Rotas aqui!</h1>
-    );
-  }
-}
-
-export default Rotas;
+//add rotas
