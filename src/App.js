3ae935214457609c1da992827687593ceb2f27b1
Wenderson Belko <contato@wendersonbelko.com>
2020-05-12 15:11:20 -0300

    flaticon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { FiLogIn } from 'react-icons/fi';
 
 function App() {
   return (
     <div>
       <h1>Hello world!</h1>
       <h2>App.js</h2>
+      <FiLogIn />
     </div>
   );
 }
