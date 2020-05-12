2f37739530d75cad031b6c75b177205c43e25099
Wenderson Belko <contato@wendersonbelko.com>
2020-05-12 15:49:41 -0300

    code climate

--- src/services/rotas.jsx
+++ src/services/rotas.jsx
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {  } from 'react-router-dom';
 
-export default Rotas Component {
+class Rotas extends React.Component {
 
   render(){
     return(
     )
   }
 }
+
+export default Rotas;
