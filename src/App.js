fb8c2fb0c5978d1ebfceaf88fb0134520e1f5a4a
Wenderson Belko <contato@wendersonbelko.com>
2020-05-12 15:37:00 -0300

    depêndencias

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import './App.css';
-import { FiLogIn } from 'react-icons/fi';
+import { FiMail } from 'react-icons/fi';
 import * as api from './services/api';
 
 class App extends React.Component {
-
-  state = {
-    
-  }
-
   componentDidMount() {
     const result = api.getCategories();
     result.then((response) => console.log(response));
@@ -19,7 +14,36 @@ class App extends React.Component {
 
   render() {
     return (
-      <h1>Hello world!</h1>
+      <div>
+        <h1>Hello world!</h1>
+        <FiMail />
+
+        <button type="button" className="btn btn-primary">Primary</button>
+
+        <ul className="nav nav-pills">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">Active</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div className="dropdown-menu" style="">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <a className="dropdown-item" href="#">Something else here</a>
+              <div className="dropdown-divider" />
+              <a className="dropdown-item" href="#">Separated link</a>
+            </div>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+
+      </div>
+
     );
   }
 }
---
+++ src/services/rotas.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {  } from 'react-router-dom';
+
+export default Rotas Component {
+
+  render(){
+    return(
+    )
+  }
+}
