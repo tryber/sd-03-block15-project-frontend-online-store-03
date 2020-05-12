276c0c243cad30568396d18aca01066da138589a
wscatao <wscatao@gmail.com>
2020-05-12 13:19:10 -0300

    cc fix

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ class App extends React.Component {
   componentDidMount() {
     const result = api.getCategories();
     result.then((response) => console.log(response));
+
+    const secondResult = api.getProductsFromCategoryAndQuery('MLB271599','Agro');
+    secondResult.then((response) => console.log(response));
   }
 
   render() {
