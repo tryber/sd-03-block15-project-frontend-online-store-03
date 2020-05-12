63a43c0e06e73a28bac104f31288e033a4a6b4eb
Wenderson Belko <contato@wendersonbelko.com>
2020-05-12 15:57:00 -0300

    code climate

--- src/services/rotas.jsx
+++ src/services/rotas.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {  } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 class Rotas extends React.Component {
-
-  render(){
-    return(
-    )
+  render() {
+    return (
+      <h1>Rotas aqui!</h1>
+    );
   }
 }
 
