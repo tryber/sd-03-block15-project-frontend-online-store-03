a4c84a705f33bea5a521201e8772ce03ecbaebf2
wscatao <wscatao@gmail.com>
2020-05-13 16:26:17 -0300

    tentando merGEAR

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Categories from '../components/categories';
 import ButtonCart from '../components/buttonCart';
-import BoxEmpty from '../components/buttonCart';
-import SearchBar from '../components/buttonCart';
+import BoxEmpty from '../components/boxEmpty';
+import SearchBar from '../components/SearchBar';
 
 class Main extends Component {
 
