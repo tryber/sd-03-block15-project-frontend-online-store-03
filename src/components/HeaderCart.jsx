cc038fb87e1269c18e023a487d5fee536ef3f446
Wenderson Belko <contato@wendersonbelko.com>
2020-05-13 15:08:36 -0300

    chamaBotao cc

--- src/components/HeaderCart.jsx
+++ src/components/HeaderCart.jsx
@@ -1,51 +1,43 @@
 import React, { Component } from 'react';
 import { MdKeyboardReturn } from 'react-icons/md';
 import { FiShoppingCart } from 'react-icons/fi';
-import { Redirect } from "react-router-dom";
+import { Redirect } from 'react-router-dom';
 
 class HeaderCart extends Component {
-
   constructor(args) {
     super(args);
-    this.state = {
-      redirect: false,
-    };
+    this.state = { redirect: false };
     this.chamaBotao = this.chamaBotao.bind(this);
   }
 
   onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+    this.setState({ [e.target.name]: e.target.value });
   }
 
-  chamaBotao = () => {
-    this.setState({
-      redirect: true,
-    });
-  };
+  chamaBotao() {
+    this.setState({ redirect: true });
+  }
 
   render() {
-      if (this.state.redirect) {
-        return <Redirect to="/" />;
-      } else {
-        return (
-          <form>
-            <button
-              id="shopping-cart-button"
-              type="submit"
-              className="btn btn-outline-success"
-              onClick={() => this.chamaBotao()}
-            >
-              <MdKeyboardReturn size="30" />
-            </button>
-            <span>
-            <FiShoppingCart />
-            Carrinho de compras
-          </span>
-          </form>
-        );
-      }
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
     }
+    return (
+      <form>
+        <button
+          id="shopping-cart-button"
+          type="submit"
+          className="btn btn-outline-success"
+          onClick={() => this.chamaBotao()}
+        >
+          <MdKeyboardReturn size="30" />
+        </button>
+        <span>
+          <FiShoppingCart />
+          Carrinho de compras
+        </span>
+      </form>
+    );
   }
+}
 export default HeaderCart;
