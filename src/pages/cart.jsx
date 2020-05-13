ff54d7ddf97469bcc4a7afa4e6b7599741a094b4
Wenderson Belko <contato@wendersonbelko.com>
2020-05-13 15:21:38 -0300

    CC

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -3,23 +3,21 @@ import BoxEmpty from '../components/boxEmpty';
 import HeaderCart from '../components/HeaderCart';
 
 export class Cart extends Component {
-
-  state = {
-    temProduto: false,
+  constructor(props) {
+    super(props);
+    this.state = { temProduto: false };
   }
 
-  render(){
-
-    return(
+  render() {
+    return (
       <div className="container">
-        <HeaderCart/>
-        {
-        this.state.temProduto ?
+        <HeaderCart />
+        {this.state.temProduto ? (
           <h1>Aqui tem que ter o carrinho!</h1>
-          :
+        ) : (
           <BoxEmpty />
-        }
+        )}
       </div>
-    )
+    );
   }
 }
