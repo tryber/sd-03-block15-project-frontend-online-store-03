fb7033ac68b1e94acda4461b9deef017cc1d722c
Wenderson Belko <contato@wendersonbelko.com>
2020-05-13 14:59:15 -0300

    teste com function CC

--- src/components/buttonCart.jsx
+++ src/components/buttonCart.jsx
@@ -1,45 +1,39 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-import { FiShoppingCart } from "react-icons/fi";
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { FiShoppingCart } from 'react-icons/fi';
 
 class ButtonCart extends Component {
   constructor(args) {
     super(args);
-    this.state = {
-      redirect: false,
-    };
+    this.state = { redirect: false };
     this.trocaState = this.trocaState.bind(this);
   }
 
   onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+    this.setState({ [e.target.name]: e.target.value });
   }
 
-  trocaState = () => {
-    this.setState({
-      redirect: true,
-    });
-  };
+  trocaState() {
+    this.setState({ redirect: true });
+  }
 
   render() {
-      if (this.state.redirect) {
-        return <Redirect to="/cart" />;
-      } else {
-        return (
-          <form>
-            <button
-              data-testid="shopping-cart-button"
-              type="submit"
-              className="btn btn-outline-success"
-              onClick={() => this.trocaState()}>
-              <FiShoppingCart size="25" />
-            </button>
-          </form>
-        );
-      }
+    if (this.state.redirect === true) {
+      return <Redirect to="/cart" />;
     }
+    return (
+      <form>
+        <button
+          data-testid="shopping-cart-button"
+          type="submit"
+          className="btn btn-outline-success"
+          onClick={() => this.trocaState()}
+        >
+          <FiShoppingCart size="25" />
+        </button>
+      </form>
+    );
   }
+}
 
-  export default ButtonCart;
+export default ButtonCart;
