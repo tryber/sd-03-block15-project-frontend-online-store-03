29860f936f45d3a6eaf1dbbce7122a9d9e3b1d75
wscatao <wscatao@gmail.com>
2020-05-14 11:35:53 -0300

    product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,17 +4,17 @@ class ProductCard extends Component {
   render() {
     const { title, thumbnail, price } = this.props.product;
     return (
-        <div
-          data-testid="product"
-          className="card bg-secondary mb-3"
-          style={{ maxWidth: 20 + "rem" }}
-        >
-          <div className="card-header">{title}</div>
-          <div className="card-body">
-            <img className="card-img" src={thumbnail} alt="product_image" />
-            <p className="card-text">{price}</p>
-          </div>
+      <div
+        data-testid="product"
+        className="card bg-secondary mb-3"
+        style={{ maxWidth: `${20}rem` }}
+      >
+        <div className="card-header">{title}</div>
+        <div className="card-body">
+          <img className="card-img" src={thumbnail} alt="product_image" />
+          <p className="card-text">{price}</p>
         </div>
+      </div>
     );
   }
 }
