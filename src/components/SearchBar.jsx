3147ad8da86ec1a21b886ad6e1b3a322e67f4aac
Wenderson Belko <contato@wendersonbelko.com>
2020-05-14 14:37:02 -0300

    projeto estilizado

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,19 @@
-import React, { Component } from "react";
-import { GoSearch } from "react-icons/go";
+import React, { Component } from 'react';
+import { GoSearch } from 'react-icons/go';
+import ProductCard from './ProductCard';
 import * as apifunc from '../services/api';
+import * as pages from '../pages/Index';
 
 export class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: "",
+      item: '',
+      searchResults: [],
     };
     this.textChange = this.textChange.bind(this);
     this.searchAPI = this.searchAPI.bind(this);
+    this.renderText = this.renderText.bind(this);
   }
 
   textChange(value) {
@@ -18,25 +22,50 @@ export class SearchBar extends Component {
 
   searchAPI(e) {
     e.preventDefault();
-    const buscar = apifunc.getProductsByTerm(this.state.item)
-    buscar.then(resposta => console.log(resposta));
+    const search = apifunc.getProductsFromCategoryAndQuery('', this.state.item);
+    search.then((answear) => this.setState({ searchResults: answear.results }));
+  }
+
+  renderText() {
+    if (this.state.item === '') {
+      return (
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      );
+    }
+    return null;
   }
 
   render() {
     return (
       <div>
-        <form>
-          <input type="text" onChange={this.textChange} />
-          <button type="submit" onClick={this.searchAPI} className="btn btn-outline-primary" >
+        <div className="bar">
+          <input
+            data-testid="query-input"
+            type="text"
+            onChange={this.textChange}
+            className="form-control search-bar"
+            placeholder="Digite sua busca aqui"
+          />
+          <button
+            data-testid="query-button"
+            type="button"
+            onClick={this.searchAPI}
+            className="btn btn-outline-primary"
+          >
             <GoSearch />
           </button>
-        </form>
-        {this.state.item === "" ? (
-          <p data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </p>
-        ) : (
-          <React.Fragment />
+          <pages.ButtonCart />
+        </div>
+
+        {this.renderText()}
+        {this.state.searchResults.length > 0 && (
+          <div className="card">
+            {this.state.searchResults.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
         )}
       </div>
     );
