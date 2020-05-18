ce24a040802b8cebbf871cb0136f437a7a6fe872
Wenderson belko <57719936+wendersonbelko@users.noreply.github.com>
2020-05-18 19:13:20 -0300

    code climate

--- src/components/Coments.jsx
+++ src/components/Coments.jsx
@@ -7,13 +7,30 @@ export class Coments extends React.Component {
       <form className="container">
         <div className="form-group">
           <label htmlFor="Email">E-mail</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="exemple@email.com" />
-          <small id="emailHelp" className="form-text text-muted">Esse email é usado apenas para verificarmos o usuário.</small>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="exemple@email.com"
+          />
+          <small
+            id="emailHelp"
+            className="form-text text-muted"
+          >
+            Esse email é usado apenas para verificarmos o usuário.
+
+          </small>
         </div>
         <Stars />
         <div className="form-group">
           <label htmlFor="exampleTextarea">Escreva seu comentário.</label>
-          <textarea data-testid="product-detail-evaluation" className="form-control" id="exampleTextarea" rows="3" />
+          <textarea
+            data-testid="product-detail-evaluation"
+            className="form-control"
+            id="exampleTextarea"
+            rows="3"
+          />
         </div>
         <button type="submit" className="btn btn-primary">Comentar</button>
       </form>
